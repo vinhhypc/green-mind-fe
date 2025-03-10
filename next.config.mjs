259ecc5ff4a0577298023a19d5693f62819d3d63
vinh.nghiemquang <vinh.nghiemquang@ivnd.com.vn>
2025-03-10 16:43:55 +0700

    init project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatar.vercel.sh',
+        port: '', // Để trống nếu không dùng port cụ thể
+        pathname: '/**', // Cho phép tất cả đường dẫn dưới hostname này
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+export const content = [
+  './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+];
+export const theme = {
+  extend: {
+    colors: {
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+    },
+    animation: {
+      marquee: 'marquee var(--duration) linear infinite',
+      'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
+    },
+    keyframes: {
+      marquee: {
+        from: { transform: 'translateX(0)' },
+        to: { transform: 'translateX(calc(-100% - var(--gap)))' },
+      },
+      'marquee-vertical': {
+        from: { transform: 'translateY(0)' },
+        to: { transform: 'translateY(calc(-100% - var(--gap)))' },
       },
     },
   },
-  plugins: [],
 };
+export const plugins = [];
